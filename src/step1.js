513582e5a35676e0417a304613788510fd339005
sugarmouse <tanghao19950911@gmail.com>
2022-09-15 17:02:56 +0800

    step0 and step1 accomplished

---
+++ src/step1.js
@@ -0,0 +1,35 @@
+const microReact = {
+  createElement
+}
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => typeof child === "object" ? child : createTextElement(child))
+    }
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }
+}
+//tell babel to transpile the JSX use the function we define
+/**@jsx microReact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+)
+
+
+const container = document.getElementById("root")
+ReactDOM.render(element, container)
